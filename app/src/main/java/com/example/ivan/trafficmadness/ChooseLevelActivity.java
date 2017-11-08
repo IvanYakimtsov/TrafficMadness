96aae680b0189b62f5e8dbf6107ef7c1a725c005
IvanYakimtsov <ivan.yakimtspv@gmail.com>
2017-11-08 14:10:48 +0300

    choose level screen modify

--- app/src/main/java/com/example/ivan/trafficmadness/ChooseLevelActivity.java
+++ app/src/main/java/com/example/ivan/trafficmadness/ChooseLevelActivity.java
@@ -2,6 +2,8 @@ package com.example.ivan.trafficmadness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ChooseLevelActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ChooseLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_level);
+        ImageButton button = (ImageButton) findViewById(R.id.back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
